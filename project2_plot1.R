66abfd0c5e561d67f70626af7cb8e288d53db567
Michael Fierro <mifierro@cisco.com>
2015-11-18 20:13:30 -0700

    Back to the past

--- project2_plot1.R
+++ project2_plot1.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 library(dplyr)
 sccPM25 <- readRDS("/home/biffster/Dropbox/Work/Training/Data science/Coursera/Exploratory Data Analysis/summarySCC_PM25.rds")
 years <- group_by(sccPM25, year)
@@ -6,13 +5,3 @@ totalemission <- summarise(years,count = sum(Emissions))
 plot(totalemission$year,totalemission$count, type = "l", main="", ylab = "PM25 Emissions (tons)", xlab = "")
 dev.copy(png,"project2_plot1.png")
 dev.off()
-||||||| merged common ancestors
-=======
-library(dplyr)
-sccPM25 <- readRDS("summarySCC_PM25.rds")
-years <- group_by(sccPM25, year)
-totalemission <- summarise(years,count = n())
-plot(totalemission$year,totalemission$count, type = "l", main="", ylab = "PM25 Emissions (tons)", xlab = "")
-dev.copy(png,"project2_plot1.png")
-dev.off()
->>>>>>> ae63e1d650067f1d6ecf80b4cf1ef479c1465ec0
