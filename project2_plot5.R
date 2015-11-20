acbd751b9a344b45fa17bca07251aa0407d58238
Michael Fierro <mifierro@cisco.com>
2015-11-19 22:26:12 -0700

    Added in Project 2 Plot 5

---
+++ project2_plot5.R
@@ -0,0 +1,12 @@
+library(dplyr)
+library(plyr)
+sccPM25 <- readRDS("/home/biffster/Dropbox/Work/Training/Data science/Coursera/Exploratory Data Analysis/summarySCC_PM25.rds")
+sourcesPM25 <- readRDS("/home/biffster/Dropbox/Work/Training/Data science/Coursera/Exploratory Data Analysis/Source_Classification_Code.rds")
+sccPM25balt <- sccPM25[which(sccPM25$fips == 24510),]
+lookupbalt <- sourcesPM25[grep("Coal", sourcesPM25$SCC.Level.Three),]
+coalFullbalt <- join(lookupbalt, sccPM25balt, by="SCC")
+coalYearsbalt <- group_by(coalFullbalt, year)
+coalTotalbalt <- dplyr::summarise(coalYearsbalt, count = sum(Emissions, na.rm = TRUE))
+plot(coalTotalbalt$year, coalTotalbalt$count, type = "l", main = "Coal combustion sources in Baltimore City", ylab = "PM25 Emissions (tons)", xlab = "")
+dev.copy(png,"project2_plot5.png")
+dev.off()
