b797958226cf70bfc76bc806b22a55de0370f3b2
Michael Fierro <mifierro@cisco.com>
2015-11-22 13:14:43 -0700

    Final Plot 1

--- project2_plot1.R
+++ project2_plot1.R
@@ -1,7 +1,22 @@
+# This script will generate a plot showing the trend of PM2.5 Emissions over
+# the decade from 1999 to 2008.
+
+# I use dplyr to use the groupby() and summarise() functions
 library(dplyr)
+
+# Import data from the RDS file into a dataframe
 sccPM25 <- readRDS("/home/biffster/Dropbox/Work/Training/Data science/Coursera/Exploratory Data Analysis/summarySCC_PM25.rds")
+
+# Group the datafiles by year, and then summarise the resulting dataset
+# to get the total emissions by year.
 years <- group_by(sccPM25, year)
 totalemission <- dplyr::summarise(years,count = sum(Emissions))
-plot(totalemission$year,totalemission$count, type = "l", main="", ylab = "PM25 Emissions (tons)", xlab = "")
+
+# Simple line plot using the base graphics system. Note: I chose to
+# express the emissions in log(tons) to better highlight the scale of
+# the trends over the years.
+plot(totalemission$year,log(totalemission$count), type = "l", main="Fine particular matter emission trends", ylab = "PM25 Emissions (log(tons))", xlab = "")
+
+# Export the plot to a PNG file.
 dev.copy(png,"project2_plot1.png")
 dev.off()
